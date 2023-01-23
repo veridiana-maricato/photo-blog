8843a34d9572f3afb39131f06bdca99f498b7d26
Veridiana Maricato <maricato.veridiana@gmail.com>
2023-01-23 13:30:51 -0300

    navbar position fixed

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const About = () => {
     return (
       <div className={styles.about}>
-          <h2>About us</h2>
+          <h1>About us</h1>
           <p>This project consist on a blog created with React and Firebase</p>
           <Link to='/posts/create' className='btn'>Create Post</Link>
       </div>
